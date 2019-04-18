3d499d2767cd3641f9f6aaf113230c5275815446
LeslieZIGAULT <lesliez-lord@hotmail.fr>
2019-04-18 20:21:17 +0200

    Update header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+ <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+<header>
+    
+    <nav>
+        <ul>
+            <li> Accueil</li>
+            <li> Articles</li>
+            <li> Commandes</li>
+            <li> Client</li>
+        </ul>
+    </nav>
+</header>
+  
+
+
+
+
+   
