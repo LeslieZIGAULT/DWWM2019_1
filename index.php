02496924e2b401420667364778953d552385d22f
LeslieZIGAULT <lesliez-lord@hotmail.fr>
2019-04-22 21:31:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php include('header.php');?>
 <?php echo "Bonjour pierre"; ?>
- 
+ L'appellation de pierre précieuse, officiellement abrogée par la législation française en janvier 20021 est une dénomination historique qui s'applique en France aux quatre gemmes que sont le diamant, le saphir, le rubis et l'émeraude.
+
+Pour mériter cette appellation, les gemmes doivent être naturelles, d'une certaine dureté, ainsi que d'une relative beauté, elles sont ainsi en général assez rares. Une partie des pierres précieuses, en raison de leurs qualités exceptionnelles (pureté, rareté, qualité ou même histoire) sont qualifiées de joyaux. On peut aujourd'hui distinguer trois principaux types de gemmes :
+
+les pierres précieuses (diamant, rubis, saphir et émeraude) ;
+les pierres fines, autrefois pierres semi-précieuses ou semi-fines, catégorie qui concerne toutes les gemmes qui ne figurent pas dans les quatre précédentes (grenat, lapis-lazuli, cornaline, jaspe, etc.). Cette catégorie est définie par une liste de la CIBJO (Confédération internationale de bijouterie, joaillerie, orfèvrerie des diamants, perles et pierres) ;
+les pierres organiques, catégorie dont font partie les gemmes issues de matière organique et non pas minérale (l'ambre, une résine fossile, le corail, formé par les colonies de polypes, la nacre, revêtement intérieur de certains mollusques, la perle, composée de nacre dont le mollusque se sert pour se protéger d'un élément étranger, le jais, un dérivé du charbon, etc.).
+
 <?php include('footer.html');?>
  
  
