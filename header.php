01b991f2b7de3dd97abd92391e652bbafc65126c
LeslieZIGAULT <lesliez-lord@hotmail.fr>
2019-04-25 15:55:02 +0200

    modif

--- header.php
+++ header.php
@@ -51,47 +51,33 @@
       </ul>
     </div>
   </div>
-</nav>
+</nav> 
+          
+ <!--   
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="#">Fixed navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline mt-2 mt-md-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>-->
            
 </header>
-  
 
-
-
-
-   
-=======
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
- </head>
-
- <body>
-
-     <header>
-
-         <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-             <div class="container-fluid">
-                 <div class="navbar-header">
-                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                         <span class="icon-bar"></span>
-                         <span class="icon-bar"></span>
-                         <span class="icon-bar"></span>
-                     </button>
-                     <a class="navbar-brand" href="#">Accueil</a>
-                 </div>
-
-                 <div class="collapse navbar-collapse" id="myNavbar">
-                     <ul class="nav navbar-nav">
-                         <li class="active"><a href="#">Portfolio</a></li>
-                         <li><a href="#">A propos</a></li>
-                         <li><a href="#">Contactez-nous</a></li>
-                     </ul>
-                     <ul class="nav navbar-nav navbar-right">
-                         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                     </ul>
-                 </div>
-             </div>
-         </nav>
-
-     </header>
->>>>>>> 62ae302dcc805ca36114a6fc2f621e6844bd3d2c
+ 
