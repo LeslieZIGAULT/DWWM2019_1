3f0ba360a7c01157684ad392bf0a3795ebd739e7
Geraldine Galvani <31082621+GeraldineCodes@users.noreply.github.com>
2019-04-26 13:17:18 -0400

    header

---
+++ header.php
@@ -0,0 +1,134 @@
+ <!DOCTYPE html>
+ <html lang="fr">
+
+ <head>
+     <meta charset="UTF-8">
+     <title>Document</title>
+
+     <!-- LIENS CSS -->
+     <link rel="stylesheet" href="css/footer.css" type="text/css">
+     <link rel="stylesheet" href="css/index.css" type="text/css">
+     <link rel="stylesheet" href="css/header.css" type="text/css">
+
+     <!-- LIENS JAVASCRIPT-->
+     <script src="script/script.js"></script>
+
+     <!--LIENS POUR LES POLICES DE CARACTERES-->
+     <link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
+
+     <!-- LIENS POUR LES ICONS FONT AWESOME -->
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+     <!-- LIENS POUR BOOTSTRAP -->
+<<<<<<< HEAD
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+ </head>
+
+ <body>
+
+     <header>
+
+        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+=======
+
+  <!--
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+-->
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+<header>
+
+  <nav class="navbar navbar-expand-sm bg-primary">
+
+    <!-- Links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Articles</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Commandes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Clients</a>
+      </li>
+    </ul>
+
+  </nav>
+
+<!--
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+>>>>>>> c110866925204a702dd3b25213dbb68532178c0f
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand navbar-light" href="#">Accueil</a>
+    </div>
+
+   <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Portfolio</a></li>
+        <li><a href="#">A propos</a></li>
+        <li><a href="#">Contactez-nous</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+<<<<<<< HEAD
+</nav> 
+
+     </header>
+=======
+</nav>
+-->
+
+ <!--
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="#">Fixed navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline mt-2 mt-md-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>-->
+
+</header>
+>>>>>>> c110866925204a702dd3b25213dbb68532178c0f
