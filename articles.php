9d959060a0f957d520b869e584e157960d34fc9e
LeslieZIGAULT <lesliez-lord@hotmail.fr>
2019-04-29 01:04:01 +0200

    paragraphe pg accueil

--- articles.php
+++ articles.php
@@ -58,23 +58,7 @@ echo "<h1 class='text-center'>Vos Articles</h1>"
 
 <?php 
 
-/*
-<form>
-  Nom:<br>
-    <input type="text" name="Nom">
-      <br>
-  Unité:<br>
-    <input type="text" name="Unité">
-      
-  Quantité:<br>
-    <input type="text" name="Quantité">
-      <br>
-      
-    <input type="submit" value="Ajouter image"> 
-      <br><br>
-    <input type="submit" value="Valider">
-</form> 
-  */    
+
 ?>
 
 <?php include('footer.html');?>
--- index.php
+++ index.php
@@ -1,27 +1,15 @@
 <?php include('header.php');?>
 
 
-<?php echo "Bonjour pierre"; ?>
 <?php
-echo "<div>
-<p> L'appellation de pierre précieuse, officiellement abrogée par la législation française en janvier 20021 est une dénomination historique qui s'applique en France aux quatre gemmes que sont le diamant, le saphir, le rubis et l'émeraude.
-
-Pour mériter cette appellation, les gemmes doivent être naturelles, d'une certaine dureté, ainsi que d'une relative beauté, elles sont ainsi en général assez rares. Une partie des pierres précieuses, en raison de leurs qualités exceptionnelles (pureté, rareté, qualité ou même histoire) sont qualifiées de joyaux. On peut aujourd'hui distinguer trois principaux types de gemmes :
-
-les pierres précieuses (diamant, rubis, saphir et émeraude) ;
-les pierres fines, autrefois pierres semi-précieuses ou semi-fines, catégorie qui concerne toutes les gemmes qui ne figurent pas dans les quatre précédentes (grenat, lapis-lazuli, cornaline, jaspe, etc.). Cette catégorie est définie par une liste de la CIBJO (Confédération internationale de bijouterie, joaillerie, orfèvrerie des diamants, perles et pierres) ;
-les pierres organiques, catégorie dont font partie les gemmes issues de matière organique et non pas minérale (l'ambre, une résine fossile, le corail, formé par les colonies de polypes, la nacre, revêtement intérieur de certains mollusques, la perle, composée de nacre dont le mollusque se sert pour se protéger d'un élément étranger, le jais, un dérivé du charbon, etc...).</p>
-</div>"
+echo "<h2> G.L Materiaux</h2>";
 ?>
 
-
-
 <?php
-echo "<h2>Bienvenue sur votre application</h2>";
+echo "
+<p> Vous devez construire votre maison, refaire votre toiture, faire une clôture, monter une cloison, pour tout cela vous avez besoin de matériel de construction. G.L Matériaux est à votre disposition.</p>
+<p>Commandez en ligne nos produit! "
 ?>
-
-
-
 
 
 <?php include('footer.html');?>
